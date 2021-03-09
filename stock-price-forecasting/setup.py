343d32068bf1cff604d9ddb095dfd87d38c40747
linux <linux@pop-os.localdomain>
2021-03-09 13:39:25 -0300

    first commmit

---
+++ stock-price-forecasting/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this project I'll show some ways to forecast stock price.',
+    author='toni_migliato',
+    license='',
+)
